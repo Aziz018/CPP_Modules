4b837dbf6652d75bba0dc4ae7161d78169dce705
Aziz018 <khatabiaziz707@gmail.com>
2024-08-28 15:33:29 +0100

    add log file

--- Module00/ex02/Account.cpp
+++ Module00/ex02/Account.cpp
@@ -6,34 +6,50 @@
 /*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 09:26:12 by aelkheta          #+#    #+#             */
-/*   Updated: 2024/08/27 15:57:54 by aelkheta         ###   ########.fr       */
+/*   Updated: 2024/08/28 15:01:15 by aelkheta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include "Account.hpp"
 
-
-class entity
+/// @brief 
+class Account 
 {
-    int x;
-    int y;
+
 
 public:
-    entity() : x(0) ,y(0) {}
-    
-    void print_params()
-    {
-        std::cout << "x = " << x << " y = " << y << std::endl;
-    }
+
+	typedef Account		t;
+
+	static int	getNbAccounts( void );
+	static int	getTotalAmount( void );
+	static int	getNbDeposits( void );
+	static int	getNbWithdrawals( void );
+	static void	displayAccountsInfos( void );
+
+	Account( int initial_deposit );
+	~Account( void );
+
+	void	makeDeposit( int deposit );
+	bool	makeWithdrawal( int withdrawal );
+	int		checkAmount( void ) const;
+	void	displayStatus( void ) const;
+
+
+private:
+
+	static int	_nbAccounts;
+	static int	_totalAmount;
+	static int	_totalNbDeposits;
+	static int	_totalNbWithdrawals;
+
+	static void	_displayTimestamp( void );
+
+	int				_accountIndex;
+	int				_amount;
+	int				_nbDeposits;
+	int				_nbWithdrawals;
+
+	Account( void );
 
 };
-
-int main()
-{
-
-    entity en;
-
-    en.print_params();
-    // std::cout << "hello" << std::endl;
-}
