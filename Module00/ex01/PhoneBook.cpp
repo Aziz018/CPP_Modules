7dd75a7cb63e6c1e435f3bbdf7b9f84c3d0cc7ea
Aziz018 <khatabiaziz707@gmail.com>
2024-08-30 13:00:37 +0100

    pass 1st stage

--- Module00/ex01/PhoneBook.cpp
+++ Module00/ex01/PhoneBook.cpp
@@ -6,11 +6,12 @@
 /*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 01:03:05 by aelkheta          #+#    #+#             */
-/*   Updated: 2024/08/29 16:49:10 by aelkheta         ###   ########.fr       */
+/*   Updated: 2024/08/30 11:44:55 by aelkheta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
+#include <iomanip>
 #include <iostream>
 
 /// @brief class for Contact.
@@ -24,27 +25,49 @@ private:
 
 public:
 
+    Contact() : firstName(""), lastName(""), phoneNumber(""), nickName(""), darkestSecret("") {}
+    
     /// @brief Add new contact to the list.
-    /// @param _name Contact's name.
-    /// @param _phoneNumber Contact's phone number.
-    void setContact(const std::string& _firstName, const std::string& _phoneNumber) {
-        firstName = _firstName;
-        phoneNumber = _phoneNumber;
+    void setNewContact(const std::string& fn, const std::string& ln, const std::string& nn, const std::string& pn, const std::string& ds) {
+        firstName = fn;
+        lastName = ln;
+        nickName = nn;
+        phoneNumber = pn;
+        darkestSecret = ds;
     }
 
-    const std::string getFirstName() {
-        return firstName;
+    void displaySummary(int index) const {
+        std::cout << std::setw(10) << index << "|"
+                  << std::setw(10) << truncateField(firstName) << "|"
+                  << std::setw(10) << truncateField(lastName) << "|"
+                  << std::setw(10) << truncateField(nickName) << std::endl;
+    }
+    
+    void displayContact() const {
+        std::cout << "First Name: " << firstName << std::endl;
+        std::cout << "Last Name: " << lastName << std::endl;
+        std::cout << "Nickname: " << nickName << std::endl;
+        std::cout << "Phone Number: " << phoneNumber << std::endl;
+        std::cout << "Darkest Secret: " << darkestSecret << std::endl;
     }
 
-    const std::string getPhoneNumber() {
-        return phoneNumber;
+    private:
+    std::string truncateField(const std::string& field) const {
+        if (field.length() > 10)
+            return field.substr(0, 9) + ".";
+        return field;
+    }
+    
+    bool isEmpty() const {
+        return firstName.empty() && lastName.empty() && nickName.empty() &&
+               phoneNumber.empty() && darkestSecret.empty();
     }
 };
 
 /// @brief class for Phonebook.
 class PhoneBook {
 private:
-    Contact contact[8];
+    Contact contacts[8];
     int contactCount;
     int contactIndex;
 
@@ -54,8 +77,8 @@ public:
     /// @brief Add new contact to the list.
     /// @param name the name of the contact that would be added.
     /// @param phoneNumber the phone number of the contact that would be added.
-    void addContact(const std::string name, const std::string phoneNumber) {
-        contact[contactIndex % 8].setContact(name, phoneNumber);
+    void addContact(const Contact& newContact) {
+        contacts[contactIndex % 8] = newContact;
         contactIndex++;
         if (contactCount < 8)
             contactCount++;
@@ -63,63 +86,77 @@ public:
 
     /// @brief Display the contact if it found.
     /// @param name Name of the contact that should be displayed. 
-    void displayContact(const std::string name) {
-        std::string Spaces = "          ";
-        for (int i = 0; i < contactCount; i++)
-        {
-            if (name == contact[i].getFirstName())
-            {
-                std::cout << "┌─────────────────────────────────────────────────────────────────┐" << std::endl;
-                std::cout << "│  Index   │  first name  │ last name │ nickname │ darkest secre  │" << std::endl;
-                std::cout << "│──────────│──────────────│───────────│──────────│────────────────│" << std::endl;
-                std::cout << "│   "<< contactIndex << "      │   " << contact[i].getFirstName() << Spaces.substr(0, 10 - contact[i].getFirstName().length()) << " │ " << contact[i].getPhoneNumber() << std::endl;
-                std::cout << "└─────────────────────────────────────────────────────────────────┘" << std::endl;
-                return;
-            }
+    void displayContact() {
+        if (contactCount == 0) {
+            std::cout << "The phone book is empty." << std::endl;
+            return ;
         }
-        std::cout << "Contact not found!" << std::endl;
-    }
-
-    /// @brief Display the whole contact.
-    void displayWholeContact() {
+        std::cout << std::setw(10) << "Index" << " | "
+                  << std::setw(10) << "First Name" << " | "
+                  << std::setw(10) << "Last Name" << " | "
+                  << std::setw(10) << "Nick Name" << " | " << std::endl;
         for (int i = 0; i < contactCount; i++) {
-            std::cout << "Index      | first name     | last name | nickname | phone number | darkest secre" << std::endl;
-            std::cout << contact[i].getFirstName()  << "| " << contact[i].getPhoneNumber() << std::endl;
+            contacts[i].displaySummary(i);
+        }
+        
+        std::cout << "Enter the index of the contact to display: ";
+        int index;
+        std::cin >> index;
+        if (index < 0 || index > contactCount) {
+            std::cout << "Invalid index!" << std::endl;
+            return;
+        }
+        else {
+            contacts[index].displayContact();
         }
     }
-
 };
 
-/// @brief readline from stdin and execute the command if it exists.
-/// @param phoneBook the phonebook object to perform operations on.
-void readline(PhoneBook& phoneBook) {
-    std::string name, phoneNumber;
-    while (1) {
-        std::cout << "Please enter one of these commands (ADD, SEARCH, or EXIT):\n>>> ";
-        std::getline(std::cin, name);
-        if (name == "EXIT" || std::cin.eof())
-            break;
-        else if (name == "ADD") {
-            std::cout << "Please enter the contact's name:\n>>> ";
-            std::getline(std::cin, name);
-            std::cout << "Please enter the phone number:\n>>> ";
-            std::getline(std::cin, phoneNumber);
-            phoneBook.addContact(name, phoneNumber);
-        }
-        else if (name == "SEARCH") {
-            std::cout << "Please enter the contact's name to search:\n>>> ";
-            std::getline(std::cin, name);
-            phoneBook.displayContact(name);
-        }
-        else if (name == "DISPLAY")
-            phoneBook.displayWholeContact();
-        else
-            std::cout << "Command not found: `" << name << "' try again!" << std::endl;
+void addNewContact(PhoneBook& phoneBook) {
+    std::string firstName, lastName, nickName, phoneNumber, darkestSecret;
+    
+    std::cout << "Enter first name: ";
+    std::getline(std::cin, firstName);
+    std::cout << "Enter last name: ";
+    std::getline(std::cin, lastName);
+    std::cout << "Enter nick name: ";
+    std::getline(std::cin, nickName);
+    std::cout << "Enter phone number: ";
+    std::getline(std::cin, phoneNumber);
+    std::cout << "Enter darkest secret: ";
+    std::getline(std::cin, darkestSecret);
+
+    if (firstName.empty() || lastName.empty() || nickName.empty() || phoneNumber.empty() || darkestSecret.empty()) {
+        std::cout << "There is an empty field!" << std::endl;
     }
+
+    Contact newContact;
+    newContact.setNewContact(firstName, lastName, nickName, phoneNumber, darkestSecret);
+    phoneBook.addContact(newContact);
+    std::cout << "Contact added successfully!" << std::endl;
 }
 
 int main() {
     PhoneBook phoneBook;
-    readline(phoneBook);
-    return 0;
+    std::string command;
+
+    while (true) {
+        std::cout << "Enter a command (ADD, SEARCH, EXIT): ";
+        std::getline(std::cin, command);
+
+        if (command == "ADD") {
+            addNewContact(phoneBook);
+        }
+        else if (command == "SEARCH") {
+            phoneBook.displayContact();
+        }
+        else if (command == "EXIT" || std::cin.eof()) {
+            break;
+        }
+        else {
+            std::cout << "Invalid command!" << std::endl;
+        }
+    }
+    
+    return 0; 
 }
