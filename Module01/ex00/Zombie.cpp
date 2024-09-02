85c34e3ba88f30e2a1be978ef78db945b9daff10
Aziz018 <khatabiaziz707@gmail.com>
2024-09-02 07:21:29 +0100

    M01 ex00 done

--- Module01/ex00/Zombie.cpp
+++ Module01/ex00/Zombie.cpp
@@ -6,7 +6,46 @@
 /*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 18:33:01 by aelkheta          #+#    #+#             */
-/*   Updated: 2024/08/26 18:33:02 by aelkheta         ###   ########.fr       */
+/*   Updated: 2024/09/02 07:20:07 by aelkheta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "Zombie.hpp"
+
+// class Zombie {
+// private:
+//     std::string name;
+
+// public:
+
+//     // Zombie () : name (0) {}
+    
+//     Zombie (std::string& name) {
+//         this->name = name;    
+//     }
+    
+//     ~Zombie() {
+//         std::cout << "Zombie: " << this->name << " is Dead !" << std::endl;
+//     }
+    
+//     // void setName( std::string& name ) {
+//     //     this->name = name;
+//     // }
+
+//     void announce( void ) {
+//         std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+//     }
+// };
+
+
+Zombie::Zombie (std::string& name) {
+    this->name = name;    
+}
+    
+Zombie::~Zombie() {
+    std::cout << "Zombie: " << this->name << " is Dead !" << std::endl;
+}
+
+void Zombie::announce( void ) {
+        std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+    }
--- Module01/ex00/main.cpp
+++ Module01/ex00/main.cpp
@@ -6,36 +6,11 @@
 /*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 18:32:56 by aelkheta          #+#    #+#             */
-/*   Updated: 2024/09/02 02:52:01 by aelkheta         ###   ########.fr       */
+/*   Updated: 2024/09/02 07:20:35 by aelkheta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "Zombie.hpp"
-
-class Zombie {
-private:
-    std::string name;
-
-public:
-
-    // Zombie () : name (0) {}
-    
-    Zombie (std::string& name) {
-        this->name = name;    
-    }
-    
-    ~Zombie() {
-        std::cout << "Zombie: " << this->name << " is Dead !" << std::endl;
-    }
-    
-    // void setName( std::string& name ) {
-    //     this->name = name;
-    // }
-
-    void announce( void ) {
-        std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
-    }
-};
 
 int main() {
     Zombie* zooombie = newZombie("aziz");
