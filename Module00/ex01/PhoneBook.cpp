184d5d2f7186c3ff27a5b60212c0d745de2b850f
Aziz018 <khatabiaziz707@gmail.com>
2024-11-29 12:04:50 +0100

    M00 E01 done

--- Module00/ex01/PhoneBook.cpp
+++ Module00/ex01/PhoneBook.cpp
@@ -6,140 +6,123 @@
 /*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 01:03:05 by aelkheta          #+#    #+#             */
-/*   Updated: 2024/09/19 00:34:39 by aelkheta         ###   ########.fr       */
+/*   Updated: 2024/11/29 12:04:22 by aelkheta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <iostream>
-# include <iomanip>
-# include <string>
-# include <cstdlib>
+#include <PhoneBook.hpp>
 
-class Contact {
-private:
-    std::string firstName;
-    std::string lastName;
-    std::string nickname;
-    std::string phoneNumber;
-    std::string darkestSecret;
-
-public:
-    Contact() : firstName(""), lastName(""), nickname(""), phoneNumber(""), darkestSecret("") {}
-
-    void setContact(const std::string& fn, const std::string& ln, const std::string& nn,
-                    const std::string& pn, const std::string& ds) {
-        firstName = fn;
-        lastName = ln;
-        nickname = nn;
-        phoneNumber = pn;
-        darkestSecret = ds;
-    }
-
-    void displaySummary(int index) const {
-        std::cout << std::setw(10) << index << " | "
-                  << std::setw(10) << truncateField(firstName) << " | "
-                  << std::setw(10) << truncateField(lastName) << " | "
-                  << std::setw(10) << truncateField(nickname) << std::endl;
-    }
-
-    void displayFull() const {
-        std::cout << "First name: ....... " << firstName << std::endl;
-        std::cout << "Last name: ........ " << lastName << std::endl;
-        std::cout << "Nick name: ........ " << nickname << std::endl;
-        std::cout << "Phone number: ..... " << phoneNumber << std::endl;
-        std::cout << "Darkest secret: ... " << darkestSecret << std::endl;
-    }
-
-private:
-    std::string truncateField(const std::string& field) const {
-        if (field.length() > 10)
-            return field.substr(0, 9) + ".";
-        return field;
-    }
-};
-
-class PhoneBook {
-private:
-    Contact contacts[8];
-    int contactCount;
-    int nextIndex;
-
-public:
-    PhoneBook() : contactCount(0), nextIndex(0) {}
-
-    void addContact(const Contact& newContact) {
-        contacts[nextIndex] = newContact;
-        nextIndex = (nextIndex + 1) % 8;
-        if (contactCount < 8) {
-            contactCount++;
+bool isNumbers (std::string& str) {
+    for (size_t i = 0; i < str.length(); i++) {
+        if (!std::isdigit(str[i])) {
+            return (false);
         }
     }
+    return (true);
+}
 
-    void searchContact() const {
-        if (contactCount == 0) {
-            std::cout << "The phonebook is empty." << std::endl;
-            return;
-        }
+std::string Contact::truncateField(const std::string& field) const {
+    if (field.length() > 10)
+        return field.substr(0, 9) + ".";
+    return field;
+}
 
-        std::cout << std::setw(10) << "Index" << " | "
-                  << std::setw(10) << "First Name" << " | "
-                  << std::setw(10) << "Last Name" << " | "
-                  << std::setw(10) << "Nick Name" << std::endl;
-        for (int i = 0; i < contactCount; ++i) {
-            contacts[i].displaySummary(i);
-        }
+void Contact::setContact(const std::string& fn, const std::string& ln, const std::string& nn,
+                const std::string& pn, const std::string& ds) {
+    firstName = fn;
+    lastName = ln;
+    nickname = nn;
+    phoneNumber = pn;
+    darkestSecret = ds;
+}
 
+void Contact::displaySummary(int index) const {
+    std::cout << std::setw(10) << index << " | "
+                << std::setw(10) << truncateField(firstName) << " | "
+                << std::setw(10) << truncateField(lastName) << " | "
+                << std::setw(10) << truncateField(nickname) << std::endl;
+}
+
+void Contact::displayFull() const {
+    std::cout << std::endl;
+    std::cout << "First name: ....... " << firstName << std::endl;
+    std::cout << "Last name: ........ " << lastName << std::endl;
+    std::cout << "Nick name: ........ " << nickname << std::endl;
+    std::cout << "Phone number: ..... " << phoneNumber << std::endl;
+    std::cout << "Darkest secret: ... " << darkestSecret << std::endl;
+}
+
+void PhoneBook::addContact(const Contact& newContact) {
+    contacts[nextIndex] = newContact;
+    nextIndex = (nextIndex + 1) % 8;
+    if (contactCount < 8) {
+        contactCount++;
+    }
+}
+
+void PhoneBook::searchContact() const {
+    if (contactCount == 0) {
+        std::cout << "The phonebook is empty." << std::endl;
+        return;
+    }
+
+    std::cout << std::setw(10) << "Index" << " | "
+                << std::setw(10) << "First Name" << " | "
+                << std::setw(10) << "Last Name" << " | "
+                << std::setw(10) << "Nick Name" << std::endl;
+    for (int i = 0; i < contactCount; ++i) {
+        contacts[i].displaySummary(i);
+    }
+
+    int index;
+    std::string line;
+    while (true) {
         std::cout << "Enter the index of the contact to display: ";
-        int index;
-        std::cin >> index;
+
+        std::getline(std::cin, line);
+        index = std::atoi(line.c_str());
         if (std::cin.eof())
-            std::exit(0);
-        if (index < 0 || index >= contactCount) {
+        {
+            std::cin.clear();
+            std::exit(EXIT_SUCCESS);
+        }
+        if (!isNumbers(line) || index < 0 || index >= contactCount) {
             std::cout << "Invalid index!" << std::endl;
-        } else {
-            std::cout << std::endl;
+            continue;
+        }
+        else {
             contacts[index].displayFull();
             std::cout << std::endl;
+            break;
         }
     }
-};
+}
+
+void readLine(const std::string& prompt, std::string& line)
+{
+    while(true) {
+        std::cout << prompt;
+        std::getline(std::cin, line);
+                   
+        if (std::cin.eof()) {
+            std::cin.clear();
+            std::cout << "\nBye Bye!" << std::endl;
+            std::exit(EXIT_SUCCESS);
+        }
+        if (!line.empty())
+            break;
+        std::cerr << "Error: please try again!" << std::endl;
+    }
+}
 
 void addNewContact(PhoneBook& phoneBook) {
     std::string firstName, lastName, nickname, phoneNumber, darkestSecret;
 
-    std::cout << "Enter first name: ";
-    std::getline(std::cin, firstName);
-    
-    if (!firstName.empty()) {
-        std::cout << "Enter last name: ";
-        std::getline(std::cin, lastName);
-    }
-    
-    if (!lastName.empty()) {
-        std::cout << "Enter nickname: ";
-        std::getline(std::cin, nickname);
-    }
-    
-    if (!nickname.empty()) {
-        std::cout << "Enter phone number: ";
-        std::getline(std::cin, phoneNumber);
-    }
-    
-    if (!phoneNumber.empty()) {
-        std::cout << "Enter darkest secret: ";
-        std::getline(std::cin, darkestSecret);
-    }
-    
-    if (std::cin.eof()) {
-        std::cin.clear();
-        std::exit(0);
-    }
-    
-    if (firstName.empty() || lastName.empty() || nickname.empty() ||
-        phoneNumber.empty() || darkestSecret.empty()) {
-        std::cout << "There is an empty field!" << std::endl;
-        return;
-    }
+    readLine("Enter first name: ", firstName);
+    readLine("Enter last name: ", lastName);
+    readLine("Enter nickname: ", nickname);
+    readLine("Enter phone number: ", phoneNumber);
+    readLine("Enter darkest secret: ", darkestSecret);
 
     Contact newContact;
     newContact.setContact(firstName, lastName, nickname, phoneNumber, darkestSecret);
@@ -160,9 +143,9 @@ int main() {
         }
         else if (command == "SEARCH") {
             phoneBook.searchContact();
-            continue;
         }
         else if (command == "EXIT" || std::cin.eof()) {
+            std::cout << "\nBye Bye!" << std::endl;
             break;
         }
         else {
---
+++ Module00/ex01/PhoneBook.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/29 11:53:57 by aelkheta          #+#    #+#             */
+/*   Updated: 2024/11/29 12:04:30 by aelkheta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# pragma once
+
+# include <iostream>
+# include <iomanip>
+# include <string>
+# include <cstdlib>
+
+class Contact {
+private:
+    std::string firstName;
+    std::string lastName;
+    std::string nickname;
+    std::string phoneNumber;
+    std::string darkestSecret;
+
+public:
+    Contact() : firstName(""), lastName(""), nickname(""), phoneNumber(""), darkestSecret("") {}
+
+    void setContact(const std::string& fn, const std::string& ln, const std::string& nn,
+                    const std::string& pn, const std::string& ds);
+    void displaySummary(int index) const ;
+    void displayFull() const ;
+
+private:
+    std::string truncateField(const std::string& field) const;
+};
+
+class PhoneBook {
+private:
+    Contact contacts[8];
+    int contactCount;
+    int nextIndex;
+
+public:
+    PhoneBook() : contactCount(0), nextIndex(0) {}
+
+    void addContact(const Contact& newContact);
+    void searchContact() const;
+   
+};
