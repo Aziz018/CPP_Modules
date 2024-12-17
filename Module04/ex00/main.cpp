82a5a9777ca0fae910b3fc6d9eeb64e68f4ee990
Aziz018 <khatabiaziz707@gmail.com>
2024-12-17 11:58:13 +0100

    fix some problems in ex01

--- Module04/ex00/main.cpp
+++ Module04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 20:24:32 by aelkheta          #+#    #+#             */
-/*   Updated: 2024/12/17 11:48:23 by aelkheta         ###   ########.fr       */
+/*   Updated: 2024/12/17 11:58:02 by aelkheta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,27 @@
 
 int main()
 {
-    // const Animal* meta = new Animal();
-    // const Animal* j = new Dog();
-    const WrongAnimal* i = new WrongCat(); 
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
 
-    // std::cout << j->getType() << std::endl;
+    std::cout << std::endl;
+
+    const WrongAnimal* wrong = new WrongCat();
+    std::cout << wrong->getType() << std::endl;
+    wrong->makeSound();
+
+    std::cout << std::endl;
+
+    std::cout << j->getType() << std::endl;
     std::cout << i->getType() << std::endl;
     i->makeSound(); // will output the cat sound!
-    // j->makeSound(); // will output the dog sound!
-    // meta->makeSound();
+    j->makeSound(); // will output the dog sound!
+    meta->makeSound();
     
+    delete meta;
     delete i;
-    // delete j;
-    // delete meta;
+    delete j;
 
     return 0;
 }
