304c9384d71fd2566063c1947eccb730728e55fc
Aziz018 <khatabiaziz707@gmail.com>
2025-01-03 20:14:23 +0100

    ex02 add makefile

--- Module05/ex02/main.cpp
+++ Module05/ex02/main.cpp
@@ -6,7 +6,13 @@
 /*   By: aelkheta@student.1337.ma <aelkheta>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 18:25:42 by aelkheta@st       #+#    #+#             */
-/*   Updated: 2025/01/02 18:25:43 by aelkheta@st      ###   ########.fr       */
+/*   Updated: 2025/01/03 20:12:13 by aelkheta@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include <iostream>
+
+int main() {
+
+    return 0;
+}
