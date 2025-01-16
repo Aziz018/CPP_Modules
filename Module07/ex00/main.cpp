63b7c483453dbb346a5366165d13dc51f5627718
Aziz018 <khatabiaziz707@gmail.com>
2025-01-16 14:48:10 +0100

    day 07 ex00 done

--- Module07/ex00/main.cpp
+++ Module07/ex00/main.cpp
@@ -6,7 +6,68 @@
 /*   By: aelkheta@student.1337.ma <aelkheta>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 18:15:10 by aelkheta@st       #+#    #+#             */
-/*   Updated: 2025/01/13 18:15:11 by aelkheta@st      ###   ########.fr       */
+/*   Updated: 2025/01/16 14:47:31 by aelkheta@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <whatever.hpp>
+
+// ========================== subject main ========================== //
+
+int main( void ) {
+    int a = 2;
+    int b = 3;
+
+    ::swap( a, b );
+
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+
+    ::swap(c, d);
+
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
+}
+
+// ========================== my tests ========================== //
+
+// int main() {
+//     int a_int = 10;
+//     int b_int = 20;
+
+//     double a_d = 10.21;
+//     double b_d = 21.10;
+
+//     std::cout << "--------> Before swap <--------" << std::endl;
+//     std::cout << std::endl;
+
+//     std::cout << "  -> int a: " << a_int << std::endl
+//               << "  -> int b: " << b_int << std::endl;
+
+//     std::cout << "  -> double a: " << a_d << std::endl
+//               << "  -> double b: " << b_d << std::endl;
+
+//     swap(a_int, b_int);
+//     swap(a_d, b_d);
+    
+//     std::cout << std::endl;
+//     std::cout << "---------> After swap <---------" << std::endl;
+//     std::cout << std::endl;
+
+//     std::cout << "  -> int a: " << a_int << std::endl
+//               << "  -> int b: " << b_int << std::endl;
+    
+//     std::cout << "  -> double a: " << a_d << std::endl
+//               << "  -> double b: " << b_d << std::endl;
+
+//     std::cout << std::endl;
+//     std::cout << "------------> End <------------" << std::endl;
+
+//     return 0;
+// }
--- Module07/ex00/whatever.hpp
+++ Module07/ex00/whatever.hpp
@@ -6,7 +6,30 @@
 /*   By: aelkheta@student.1337.ma <aelkheta>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 18:15:17 by aelkheta@st       #+#    #+#             */
-/*   Updated: 2025/01/13 18:15:18 by aelkheta@st      ###   ########.fr       */
+/*   Updated: 2025/01/16 14:44:28 by aelkheta@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WHATERVER_HPP
+#define WHATERVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b) {
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T min(T a, T b) {
+    return a < b ? a : b;
+}
+
+template <typename T>
+T max(T a, T b) {
+    return a < b ? b : a;
+}
+
+#endif // WHATERVER_HPP
