58f7e1a9e0997c13be7690da9dc4394c923f2445
Aziz018 <khatabiaziz707@gmail.com>
2025-01-19 16:54:11 +0100

    day00 ex00

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -3,10 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aelkheta@student.1337.ma <aelkheta>        +#+  +:+       +#+        */
+/*   By: heisenberg <heisenberg@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 09:43:16 by aelkheta@st       #+#    #+#             */
-/*   Updated: 2025/01/17 09:43:17 by aelkheta@st      ###   ########.fr       */
+/*   Updated: 2025/01/19 16:52:02 by heisenberg       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASY_FIND_HPP
+#define EASY_FIND_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <stdexcept>
+
+template <typename T>
+typename T::iterator easyfind(T& container, int value)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+    if (it == container.end()) {
+        throw std::runtime_error("no occurrence is found");
+    }
+    return it;
+}
+
+#endif // EASY_FIND_HPP
--- Module08/ex00/main.cpp
+++ Module08/ex00/main.cpp
@@ -3,10 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aelkheta@student.1337.ma <aelkheta>        +#+  +:+       +#+        */
+/*   By: heisenberg <heisenberg@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 09:43:13 by aelkheta@st       #+#    #+#             */
-/*   Updated: 2025/01/17 09:43:14 by aelkheta@st      ###   ########.fr       */
+/*   Updated: 2025/01/19 16:52:54 by heisenberg       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <vector>
+#include <list>
+#include "easyfind.hpp"
+
+int main() {
+    std::vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(34);
+    vec.push_back(3);
+
+    try {
+        std::vector<int>::iterator it = easyfind(vec, 2);
+        std::cout << "Found: " << *it << std::endl;
+    } catch (std::exception &e) {
+        std::cout << "Value not found!" << std::endl;
+    }
+
+    return 0;
+}
