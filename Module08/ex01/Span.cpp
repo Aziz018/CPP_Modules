af12287c4f34a3c654010740869f3531a6481853
Aziz018 <khatabiaziz707@gmail.com>
2025-01-28 15:02:12 +0100

    start Ex01

--- Module08/ex01/Span.cpp
+++ Module08/ex01/Span.cpp
@@ -3,10 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   Span.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aelkheta@student.1337.ma <aelkheta>        +#+  +:+       +#+        */
+/*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 10:02:11 by aelkheta@st       #+#    #+#             */
-/*   Updated: 2025/01/25 10:02:12 by aelkheta@st      ###   ########.fr       */
+/*   Updated: 2025/01/28 15:01:58 by aelkheta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <Span.hpp>
+
+Span::Span(unsigned int n) : _n(n)
+{
+}
+
+void addNumber() {
+    
+}
+
+int shortestSpan() {
+    
+}
+
+int longestSpan() {
+    
+}
+
+Span::~Span()
+{
+}
--- Module08/ex01/Span.hpp
+++ Module08/ex01/Span.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Span.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aelkheta@student.1337.ma <aelkheta>        +#+  +:+       +#+        */
+/*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 10:02:14 by aelkheta@st       #+#    #+#             */
-/*   Updated: 2025/01/25 12:07:53 by aelkheta@st      ###   ########.fr       */
+/*   Updated: 2025/01/28 14:44:51 by aelkheta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,15 @@
 class Span
 {
 private:
+    unsigned int _progress;
     unsigned int _n;
     
 public:
     Span(unsigned int n);
     void addNumber();
-    void shortestSpan();
-    void longestSpan();
+    int shortestSpan();
+    int longestSpan();
     ~Span();
 };
-
-Span::Span(unsigned int n)
-{
-}
-
-Span::~Span()
-{
-}
-
 
 #endif // SPAN_HPP
