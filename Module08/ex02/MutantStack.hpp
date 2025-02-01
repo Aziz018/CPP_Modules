5e2cbd6a9d2e4b5b9271b1433e8a02396c03052a
Aziz018 <khatabiaziz707@gmail.com>
2025-02-01 12:12:07 +0100

    m08 starting ex02

--- Module08/ex02/MutantStack.hpp
+++ Module08/ex02/MutantStack.hpp
@@ -6,13 +6,47 @@
 /*   By: aelkheta@student.1337.ma <aelkheta>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 14:42:25 by aelkheta@st       #+#    #+#             */
-/*   Updated: 2025/01/29 14:48:08 by aelkheta@st      ###   ########.fr       */
+/*   Updated: 2025/02/01 11:51:28 by aelkheta@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <vector>
 
 template <typename T>
 class MutantStack {
-    
+    private:
+        std::vector<T> _array;
+        
+
+    public:
+        void push(T x);
+        void top();
+        void pop();
+        void size();
 };
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const MutantStack<T>& stack) {
+    return os;
+}
+
+template <typename T>
+void MutantStack<T>::push(T x) {
+    
+}
+
+template <typename T>
+void MutantStack<T>::top() {
+    
+}
+
+template <typename T>
+void MutantStack<T>::pop() {
+    
+}
+
+template <typename T>
+void MutantStack<T>::size() {
+    
+}
--- Module08/ex02/main.cpp
+++ Module08/ex02/main.cpp
@@ -6,41 +6,58 @@
 /*   By: aelkheta@student.1337.ma <aelkheta>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 14:42:23 by aelkheta@st       #+#    #+#             */
-/*   Updated: 2025/01/29 14:47:13 by aelkheta@st      ###   ########.fr       */
+/*   Updated: 2025/02/01 12:11:47 by aelkheta@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stack>
 #include <MutantStack.hpp>
 
 int main()
 {
-    MutantStack<int> mstack;
+    std::stack<int> stack;
+    
+    stack.push(10);
+    stack.push(11);
+    stack.push(12);
+    stack.push(13);
+    stack.push(14);
 
-    mstack.push(5);
-    mstack.push(17);
-
-    std::cout << mstack.top() << std::endl;
-
-    mstack.pop();
-
-    std::cout << mstack.size() << std::endl;
-
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    //[...]
-    mstack.push(0);
-
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-
-    ++it;
-    --it;
-    while (it != ite)
-    {
-    std::cout << *it << std::endl;
-    ++it;
+    for (; !stack.empty() ;) {
+        std::cout << stack.top() << std::endl;
+        stack.pop();
     }
-    std::stack<int> s(mstack);
-    return 0;
 }
+
+// int main()
+// {
+//     MutantStack<int> mstack;
+
+//     mstack.push(5);
+//     mstack.push(17);
+
+//     std::cout << mstack.top() << std::endl;
+
+//     mstack.pop();
+
+//     std::cout << mstack.size() << std::endl;
+
+//     mstack.push(3);
+//     mstack.push(5);
+//     mstack.push(737);
+//     //[...]
+//     mstack.push(0);
+
+//     MutantStack<int>::iterator it = mstack.begin();
+//     MutantStack<int>::iterator ite = mstack.end();
+
+//     ++it;
+//     --it;
+//     while (it != ite)
+//     {
+//         std::cout << *it << std::endl;
+//         ++it;
+//     }
+//     std::stack<int> s(mstack);
+//     return 0;
+// }
