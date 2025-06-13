71dc82c2c30c1c8c40bf28abdd4c71e89628e682
Aziz018 <khatabiaziz707@gmail.com>
2025-06-13 11:13:39 +0100

    adding jacobsthal ..

--- Module09/ex02/mergsort.cpp
+++ Module09/ex02/mergsort.cpp
@@ -18,17 +18,68 @@ void fordJohnson(std::vector<int>& arr) {
             pairs.push_back(std::make_pair(arr[i], arr[i + 1]));
     }
 
+    std::cout << std::endl;
     for ( size_t i = 0; i < pairs.size(); ++i ) {
-        winner.push_back(pairs[i].first);
-        std::cout << pairs[i].first << " ";
+        std::cout << "(" << pairs[i].first << "," << pairs[i].second << ") ";
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    std::sort(pairs.begin(), pairs.end());
+
+    for ( size_t i = 0; i < pairs.size(); ++i ) {
+        std::cout << "(" << pairs[i].first << "," << pairs[i].second << ") ";
     }
     std::cout << std::endl;
 
+    for ( size_t i = 0; i < pairs.size(); ++i ) {
+        winner.push_back(pairs[i].first);
+        looser.push_back(pairs[i].second);
+    }
+
+    std::cout << std::endl;
+
+    // for ( size_t i = 0; i < winner.size(); ++i ) {
+    //     std::cout << winner[i] << " ";
+    // }
+    // std::cout << std::endl;
+    // for ( size_t i = 0; i < looser.size(); ++i ) {
+    //     std::cout << looser[i] << " ";
+    // }
+
+    for (size_t i = 0; i < looser.size(); ++i)
+        winner.insert(std::upper_bound(winner.begin(), winner.end(), looser[i]), looser[i]);
+
+    for ( size_t i = 0; i < winner.size(); ++i ) {
+        std::cout << winner[i] << " ";
+    }
+    // std::cout << std::endl;
+    // for ( size_t i = 0; i < looser.size(); ++i ) {
+    //     std::cout << looser[i] << " ";
+    // }
+
+    std::cout << std::endl;
+    std::cout << std::endl;
 }
 
-int main() {
-    int array[] = {5, 1, 6, 2, 7, 0, 4, 8, 3};
-    std::vector<int> arr(array, array + sizeof(array) / sizeof(int));
+int main(int ac, char* av[]) {
+
+    if (ac < 2) {
+        std::cerr << "Usage:\n\t" << av[0] << " [sequence of numbers]" << std::endl;
+        return 1;
+    }
+
+    char *end;
+    std::vector<int> arr;
+    for ( int i = 1; i < ac; ++i ) {
+        int element = std::strtol(av[i], &end, 10);
+        if (*end != '\0') {
+            std::cerr << "Invalid element: " << av[i] << std::endl;
+            return 1;
+        }
+        // std::cout << i << ": " << element << std::endl;
+        arr.push_back(element);
+    }
 
     fordJohnson(arr);
 }
