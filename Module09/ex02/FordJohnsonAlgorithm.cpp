311ea864782fa2a4bedad52f6f44a0cac927d2cc
Aziz018 <khatabiaziz707@gmail.com>
2025-06-13 08:42:13 +0100

    ford-johnson algo

--- Module09/ex02/FordJohnsonAlgorithm.cpp
+++ Module09/ex02/FordJohnsonAlgorithm.cpp
@@ -92,76 +92,76 @@ bool isContainerSorted(const T& container) {
     return true;
 }
 
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        std::cerr << "Usage: ./PmergeMe <positive integers>" << std::endl;
-        return 1;
-    }
+// int main(int argc, char** argv) {
+//     if (argc < 2) {
+//         std::cerr << "Usage: ./PmergeMe <positive integers>" << std::endl;
+//         return 1;
+//     }
 
-    // std::cout << "[1] Parsing and validating input..." << std::endl;
-    std::vector<int> input;
-    std::set<int> seen;
+//     // std::cout << "[1] Parsing and validating input..." << std::endl;
+//     std::vector<int> input;
+//     std::set<int> seen;
 
-    for (int i = 1; i < argc; ++i) {
-        std::string arg(argv[i]);
-        for (size_t j = 0; j < arg.length(); ++j) {
-            if (!isdigit(arg[j])) {
-                // std::cerr << "Error: Non-digit input" << std::endl;
-                std::cerr << "Error:" << std::endl;
-                return 1;
-            }
-        }
+//     for (int i = 1; i < argc; ++i) {
+//         std::string arg(argv[i]);
+//         for (size_t j = 0; j < arg.length(); ++j) {
+//             if (!isdigit(arg[j])) {
+//                 // std::cerr << "Error: Non-digit input" << std::endl;
+//                 std::cerr << "Error:" << std::endl;
+//                 return 1;
+//             }
+//         }
 
-        int num = std::atoi(argv[i]);
-        if (num < 0) {
-            // std::cerr << "Error: Negative input" << std::endl;
-            std::cerr << "Error:" << std::endl;
-            return 1;
-        }
+//         int num = std::atoi(argv[i]);
+//         if (num < 0) {
+//             // std::cerr << "Error: Negative input" << std::endl;
+//             std::cerr << "Error:" << std::endl;
+//             return 1;
+//         }
 
-        if (seen.count(num)) {
-            std::cerr << "Error:" << std::endl;
-            // std::cerr << "Error: Duplicate input" << std::endl;
-            return 1;
-        }
+//         if (seen.count(num)) {
+//             std::cerr << "Error:" << std::endl;
+//             // std::cerr << "Error: Duplicate input" << std::endl;
+//             return 1;
+//         }
 
-        seen.insert(num);
-        input.push_back(num);
-    }
+//         seen.insert(num);
+//         input.push_back(num);
+//     }
 
-    // std::cout << "[2] Initial input:" << std::endl << "Before: ";
-    std::cout << "Before: ";
-    print_container(input);
+//     // std::cout << "[2] Initial input:" << std::endl << "Before: ";
+//     std::cout << "Before: ";
+//     print_container(input);
 
-    // std::cout << "[3] Copying input into containers..." << std::endl;
-    std::vector<int> vec(input.begin(), input.end());
-    std::deque<int> deq(input.begin(), input.end());
+//     // std::cout << "[3] Copying input into containers..." << std::endl;
+//     std::vector<int> vec(input.begin(), input.end());
+//     std::deque<int> deq(input.begin(), input.end());
 
-    // std::cout << "[4] Sorting with std::vector..." << std::endl;
-    long startVec = gettime();
-    merge_sort_vector(vec);
-    long endVec = gettime();
+//     // std::cout << "[4] Sorting with std::vector..." << std::endl;
+//     long startVec = gettime();
+//     merge_sort_vector(vec);
+//     long endVec = gettime();
 
-    // std::cout << "[5] Sorting with std::deque..." << std::endl;
-    long startDeq = gettime();
-    merge_sort_deque(deq);
-    long endDeq = gettime();
+//     // std::cout << "[5] Sorting with std::deque..." << std::endl;
+//     long startDeq = gettime();
+//     merge_sort_deque(deq);
+//     long endDeq = gettime();
     
-    // std::cout << "[6] Sorted result:" << std::endl << "After: ";
-    std::cout << "After: ";
-    print_container(vec);
-    // isContainerSorted(vec);
+//     // std::cout << "[6] Sorted result:" << std::endl << "After: ";
+//     std::cout << "After: ";
+//     print_container(vec);
+//     // isContainerSorted(vec);
 
-    // std::cout << "After: ";
-    // print_container(deq);
-    // isContainerSorted(deq);
+//     // std::cout << "After: ";
+//     // print_container(deq);
+//     // isContainerSorted(deq);
 
-    double timeVec = static_cast<double>(endVec - startVec);
-    double timeDeq = static_cast<double>(endDeq - startDeq);
+//     double timeVec = static_cast<double>(endVec - startVec);
+//     double timeDeq = static_cast<double>(endDeq - startDeq);
 
-    // std::cout << "[7] Timing results:" << std::endl;
-    std::cout << "Time to process a range of " << vec.size() << " elements with std::vector : " << timeVec << " us" << std::endl;
-    std::cout << "Time to process a range of " << deq.size() << " elements with std::deque  : " << timeDeq << " us" << std::endl;
+//     // std::cout << "[7] Timing results:" << std::endl;
+//     std::cout << "Time to process a range of " << vec.size() << " elements with std::vector : " << timeVec << " us" << std::endl;
+//     std::cout << "Time to process a range of " << deq.size() << " elements with std::deque  : " << timeDeq << " us" << std::endl;
 
-    return 0;
-}
+//     return 0;
+// }
